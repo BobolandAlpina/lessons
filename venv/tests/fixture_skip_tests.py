0a020d6e0d7cc17df3550ba7de08aa9c0d42ff71
BobolandAlpina <g.gubin.alpina@gmail.com>
2022-09-12 14:22:53 +0300

    New fixture modules added Module 3 exam lessons added

--- venv/tests/fixture_skip_tests.py
+++ venv/tests/fixture_skip_tests.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
@@ -21,7 +20,6 @@ class TestMainPage1():
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
-    #@pytest.mark.smoke
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
